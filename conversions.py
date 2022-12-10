904ae69e77ed8d93fb743b7c5d5b9f2b983cbfb0
ojmaster <jafferyo.123@gmail.com>
2022-12-09 19:04:37 -0600

    update requirements

--- conversions.py
+++ conversions.py
@@ -10,18 +10,18 @@ c = conn.cursor()
 # Create a table
 c.execute('''CREATE TABLE IF NOT EXISTS prefixes
                 (guild INTEGER, prefix text)''')
-    
+
 # Convert the prefixes.json file into a dictionary and insert it into the database
 """with open('prefixes.json', 'r') as f:
     prefixes = json.load(f)
     for guild in prefixes:
         c.execute("INSERT INTO prefixes VALUES (?, ?)", (guild, prefixes[guild]))"""
-    
-def change_prefix(guild, prefix):
-    c.execute("UPDATE prefixes SET prefix = ? WHERE guild = ?", (prefix, guild))
-    conn.commit()
 
-change_prefix(651230389171650560, ";")
+# def change_prefix(guild, prefix):
+#     c.execute("UPDATE prefixes SET prefix = ? WHERE guild = ?", (prefix, guild))
+#     conn.commit()
+
+# change_prefix(651230389171650560, ";")
 
 conn.commit()
 conn.close()
